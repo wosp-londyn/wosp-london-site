d5ea5fa1cb455232839e63e7fddeb603a0875e14
Neology92 <oskarlegner@gmail.com>
2019-11-16 23:37:37 +0100

    Add colors to palette of colors

---
+++ src/Assets/Styles/theme.js
@@ -0,0 +1,39 @@
+const theme = {
+    color: {
+        white: '#FFFFFF',
+        black: '#000000',
+        bg1: '#FFF',
+        bg2: '#EEE',
+        yellow: '#FAB400',
+        blue: '#009FE3',
+        green: '#96C11F',
+        red: '#E30613',
+        orange: '#EE7003',
+        navyBlue: '#212851',
+
+        rgb: {
+            white: '255,255,255',
+            black: '0,0,0',
+        },
+    },
+
+    media: {
+        above: {
+            xl: '@media (min-width: 1200px)',
+            lg: '@media (min-width: 992px)',
+            md: '@media (min-width: 768px)',
+            sm: '@media (min-width: 576px)',
+            xs: '@media (min-width: 320px)',
+        },
+
+        below: {
+            xl: '@media (min-width: 1200px)',
+            lg: '@media (min-width: 992px)',
+            md: '@media (min-width: 768px)',
+            sm: '@media (min-width: 576px)',
+            xs: '@media (min-width: 320px)',
+        },
+    },
+};
+
+export default theme;
