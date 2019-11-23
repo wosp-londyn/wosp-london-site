d70e3ff280ad5d15d899655e5b0d45713f8d2d5d
Neology92 <oskarlegner@gmail.com>
2019-11-23 22:02:24 +0100

    Add breadcrumbs to main layout

--- src/Layouts/MainLayout.js
+++ src/Layouts/MainLayout.js
@@ -1,33 +1,52 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
+import { useLocation } from 'react-router-dom';
 
-// import 'assets/styles/fontsAtma.css';
-// import 'assets/styles/fontsRoboto.css';
+// import 'assets/styles/fontsLato.css';
+// import 'assets/styles/fontsRaleway.css';
 
 import GlobalStyle from '../Assets/Styles/GlobalStyle';
 import theme from '../Assets/Styles/theme';
 import { Header, Footer, Breadcrumbs } from '../Components';
 
-const MainLayout = ({ children }) => (
-    <>
-        <ThemeProvider theme={theme}>
-            <>
-                <GlobalStyle />
-                <Header />
-                <Breadcrumbs />
-                <StyledMain>{children}</StyledMain>
-                <Footer />
-            </>
-        </ThemeProvider>
-    </>
-);
+const MainLayout = ({ children }) => {
+    const location = useLocation();
 
+    let preSlug = '';
+    if (
+        // If localhost - dont use prefix
+        window.location.hostname === 'localhost' ||
+        window.location.hostname === '127.0.0.1'
+    ) {
+        preSlug = '';
+    } else if (
+        // Prefix if hosted on github
+        window.location.hostname === 'neology92.github.io'
+    ) {
+        preSlug = '/wosp-london-site';
+    }
+
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <>
+                    <GlobalStyle />
+                    <Header />
+                    {location.pathname === `${preSlug}/` ? null : (
+                        <Breadcrumbs /> // Show breadcrumbs everywhere but homepage
+                    )}
+                    <StyledMain>{children}</StyledMain>
+                    <Footer />
+                </>
+            </ThemeProvider>
+        </>
+    );
+};
 const StyledMain = styled.main`
     width: 100vw;
     min-height: 100vh;
     /*padding: 10px;*/
     text-align: center;
-    /* background: ${({ theme }) => theme.color.bg1}; */
 `;
 
 export default MainLayout;
