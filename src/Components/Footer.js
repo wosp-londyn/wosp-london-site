5d50c33563c762055b3f91ee43872e4fa49dbd2c
unknown <kwojciechowska@edu.cdv.pl>
2019-11-24 01:26:56 +0100

    New logo heart img to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 import styled from 'styled-components';
+import heart from '../Assets/Images/logo132.png';
 
 const Footer = () => {
     return (
         <StyledContainer>
             <Row>
-                <Col md={4}>LOGO DUŻE</Col>
+                <Col md={4}>
+                    <img src={heart} id="heart" alt="wosp logo" />
+                </Col>
+
                 <Col md={5} />
+
                 <Col md={3}>Socjalmendja</Col>
             </Row>
         </StyledContainer>
@@ -18,10 +23,19 @@ const StyledContainer = styled(Container)`
     margin-top: 100px;
     background: white;
     outline: black 1px solid;
+    
+    /* background-color: ${({ theme }) => theme.color.navyBlue}; */
 
     div {
+        width: 100%;
         height: 200px;
         outline: black 1px solid;
+    } 
+    
+    #heart {
+        margin-top: 5%;
+        float: right;
+  
     }
 `;
 
