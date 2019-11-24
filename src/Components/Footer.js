dd7b1fb8d1b2a0f5b3fafee9905a2ea910e1024f
unknown <kwojciechowska@edu.cdv.pl>
2019-11-24 18:19:21 +0100

    Make 2. Row in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,15 +5,22 @@ import heart from '../Assets/Images/logo132.png';
 
 const Footer = () => {
     return (
-        <StyledContainer>
+        <StyledContainer fluid>
             <Row>
                 <Col md={4}>
-                    <img src={heart} id="heart" alt="wosp logo" />
+                    <StyleImg>
+                        <img src={heart} id="heart" alt="wosp logo" />
+                    </StyleImg>
                 </Col>
 
-                <Col md={5} />
+                <Col md={5}> </Col>
 
                 <Col md={3}>Socjalmendja</Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>Copyright HARDCODE dla Wośp Londyn</p>
+                </Col>
             </Row>
         </StyledContainer>
     );
@@ -22,21 +29,18 @@ const Footer = () => {
 const StyledContainer = styled(Container)`
     margin-top: 100px;
     background: white;
-    outline: black 1px solid;
-    
+    outline: pink 1px solid;
     /* background-color: ${({ theme }) => theme.color.navyBlue}; */
 
     div {
-        width: 100%;
-        height: 200px;
-        outline: black 1px solid;
+        outline: black px solid;
     } 
-    
-    #heart {
-        margin-top: 5%;
-        float: right;
-  
-    }
+
+`;
+
+const StyleImg = styled.div`
+    float: right;
+    outline: green 1px solid;
 `;
 
 export default Footer;
