9ad4044438ecfc69d738b6d9ff885b761d7fc72d
Neology92 <oskarlegner@gmail.com>
2019-11-25 15:43:13 +0100

    Pass sectionColor to header

--- src/Layouts/MainLayout.js
+++ src/Layouts/MainLayout.js
@@ -10,14 +10,35 @@ import theme from '../Assets/Styles/theme';
 import { Header, Footer, Breadcrumbs } from '../Components';
 
 const MainLayout = ({ children }) => {
+    const getCurrentSection = path => {
+        return path.split('/')[1];
+    };
+
+    const setColor = path => {
+        const section = getCurrentSection(path);
+
+        if (section === 'aktualnosci') {
+            return theme.color.red;
+        } if (section === 'informacje') {
+            return theme.color.blue;
+        } if (section === 'wspieraj-nas') {
+            return theme.color.orange;
+        } if (section === 'kontakt') {
+            return theme.color.yellow;
+        } 
+            return 'none';
+        
+    };
+
     const location = useLocation();
+    const sectionColor = setColor(location.pathname);
 
     return (
         <>
             <ThemeProvider theme={theme}>
                 <>
                     <GlobalStyle />
-                    <Header />
+                    <Header sectionColor={sectionColor} />
                     {location.pathname === `/` ? null : (
                         <Breadcrumbs /> // Show breadcrumbs everywhere but homepage
                     )}
