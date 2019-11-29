ab83b50bf50beb107ed1e19913e6129870ed7ae8
Neology92 <oskarlegner@gmail.com>
2019-11-29 21:46:07 +0100

    ScrollToTop after route

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import './style.css';
 import MainLayout from './Layouts/MainLayout';
 import * as views from './Layouts';
 import PostTemplate from './Templates/PostTemplate';
+import ScrollToTop from './Utils/ScrollToTop';
 
 library.add(faHome);
 
@@ -45,74 +46,76 @@ class App extends Component {
 
         return (
             <Router>
-                <MainLayout>
-                    <Switch>
-                        <Route exact path="/" component={views.Home} />
-                        <Route exact path="/aktualnosci">
-                            <Redirect to="/aktualnosci/artykuly" />
-                        </Route>
-                        <Route
-                            exact
-                            path="/aktualnosci/artykuly"
-                            component={views.Posts}
-                        />
-                        {data.map(post => (
+                <ScrollToTop>
+                    <MainLayout>
+                        <Switch>
+                            <Route exact path="/" component={views.Home} />
+                            <Route exact path="/aktualnosci">
+                                <Redirect to="/aktualnosci/artykuly" />
+                            </Route>
                             <Route
-                                key={post.id}
-                                path={`/aktualnosci/artykuly/${post.slug}`}
-                                render={props => (
-                                    <PostTemplate post={post} {...props} />
-                                )}
+                                exact
+                                path="/aktualnosci/artykuly"
+                                component={views.Posts}
                             />
-                        ))}
-                        <Route
-                            path="/aktualnosci/podcasty"
-                            component={views.Podcasts}
-                        />
-                        <Route
-                            path="/aktualnosci/live"
-                            component={views.Live}
-                        />
-                        <Route
-                            path="/aktualnosci/galeria"
-                            component={views.Gallery}
-                        />
+                            {data.map(post => (
+                                <Route
+                                    key={post.id}
+                                    path={`/aktualnosci/artykuly/${post.slug}`}
+                                    render={props => (
+                                        <PostTemplate post={post} {...props} />
+                                    )}
+                                />
+                            ))}
+                            <Route
+                                path="/aktualnosci/podcasty"
+                                component={views.Podcasts}
+                            />
+                            <Route
+                                path="/aktualnosci/live"
+                                component={views.Live}
+                            />
+                            <Route
+                                path="/aktualnosci/galeria"
+                                component={views.Gallery}
+                            />
 
-                        <Route exact path="/informacje">
-                            <Redirect to="/informacje/program" />
-                        </Route>
-                        <Route
-                            path="/informacje/program"
-                            component={views.Program}
-                        />
-                        <Route
-                            path="/informacje/gwiazdy"
-                            component={views.Stars}
-                        />
-                        <Route
-                            path="/informacje/partnerzy"
-                            component={views.Partners}
-                        />
+                            <Route exact path="/informacje">
+                                <Redirect to="/informacje/program" />
+                            </Route>
+                            <Route
+                                path="/informacje/program"
+                                component={views.Program}
+                            />
+                            <Route
+                                path="/informacje/gwiazdy"
+                                component={views.Stars}
+                            />
+                            <Route
+                                path="/informacje/partnerzy"
+                                component={views.Partners}
+                            />
 
-                        <Route exact path="/wspieraj-nas">
-                            <Redirect to="/wspieraj-nas/zostan-wolontariuszem" />
-                        </Route>
-                        <Route
-                            path="/wspieraj-nas/zostan-wolontariuszem"
-                            component={views.Volunteer}
-                        />
-                        <Route
-                            path="/wspieraj-nas/wplac-na-wosp"
-                            component={views.Support}
-                        />
-                        <Route
-                            path="/wspieraj-nas/aukcje"
-                            component={views.Auctions}
-                        />
+                            <Route exact path="/wspieraj-nas">
+                                <Redirect to="/wspieraj-nas/zostan-wolontariuszem" />
+                            </Route>
+                            <Route
+                                path="/wspieraj-nas/zostan-wolontariuszem"
+                                component={views.Volunteer}
+                            />
+                            <Route
+                                path="/wspieraj-nas/wplac-na-wosp"
+                                component={views.Support}
+                            />
+                            <Route
+                                path="/wspieraj-nas/aukcje"
+                                component={views.Auctions}
+                            />
 
-                        <Route path="/kontakt" component={views.Contact} />
-                    </Switch>
-                </MainLayout>
+                            <Route path="/kontakt" component={views.Contact} />
+                        </Switch>
+                    </MainLayout>
+                </ScrollToTop>
             </Router>
         );
     }
