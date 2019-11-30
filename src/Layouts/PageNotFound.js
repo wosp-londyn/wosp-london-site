79234f31f6fff91e07b429348e6ef39c006f5ca4
Neology92 <oskarlegner@gmail.com>
2019-11-30 13:53:50 +0100

    Add 404 page

---
+++ src/Layouts/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return (
+        <div style={{ paddingTop: '200px' }}>
+            <h1>404 Page not found.</h1>
+        </div>
+    );
+};
+
+export default PageNotFound;
