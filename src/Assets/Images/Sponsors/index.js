167f00a6d124bdf8847df049d4fa0377912007b6
Neology92 <oskarlegner@gmail.com>
2019-12-02 03:00:34 +0100

    Add content and style sidebar

---
+++ src/Assets/Images/Sponsors/index.js
@@ -0,0 +1,17 @@
+import transfergo from './transfergo.png';
+import mleczko from './mleczko.png';
+import smartchoice from './smartchoice.png';
+import ant from './ant.png';
+import piling from './piling.png';
+import emotionwraps from './emotionwraps.png';
+import galitsyna from './galitsyna.jpg';
+
+export {
+    transfergo,
+    mleczko,
+    smartchoice,
+    ant,
+    piling,
+    emotionwraps,
+    galitsyna,
+};
