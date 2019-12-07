e1642374b770c013d41b055a78e373728b44c5e8
Neology92 <oskarlegner@gmail.com>
2019-12-07 04:24:08 +0100

    Well... fuck it

--- src/Layouts/Live.js
+++ src/Layouts/Live.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Live = () => {
-    return <div>Live</div>;
+    return (
+        <div>
+            Trzeba tutaj napisać coś mądrego na temat live
+            <br />
+            <br />
+            <ul>
+                <li>#będzie</li>
+                <li>#jeszczeNieMa</li>
+                <li>#kiedyBędzie</li>
+                <li>#SieMa</li>
+            </ul>
+        </div>
+    );
 };
 
 export default Live;
