b19dde8eb198845d1309a40e3b57a8f810d0e108
Neology92 <oskarlegner@gmail.com>
2019-12-07 04:14:03 +0100

    Change styling contnet way

--- src/Layouts/Program.js
+++ src/Layouts/Program.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Tabs, Tab } from 'react-bootstrap';
 import PageTemplate from '../Templates/PageTemplate';
+
+const ContentBlock = styled.div``;
 
 const Program = () => {
     return (
         <Tabs>
             <Tab eventKey="adults" title="Dorośli">
-                <PageTemplate slug="program-dorosli" />
+                <PageTemplate
+                    slug="program-dorosli"
+                    ContentWrapper={ContentBlock}
+                />
             </Tab>
             <Tab eventKey="kids" title="Dzieci">
-                <PageTemplate slug="program-dzieci" />
+                <PageTemplate
+                    slug="program-dzieci"
+                    ContentWrapper={ContentBlock}
+                />
             </Tab>
             <Tab eventKey="events" title="Eventy">
-                <PageTemplate slug="imprezy-dodatkowe" />
+                <PageTemplate
+                    slug="imprezy-dodatkowe"
+                    ContentWrapper={ContentBlock}
+                />
             </Tab>
         </Tabs>
     );
--- src/Templates/PageTemplate.js
+++ src/Templates/PageTemplate.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import Loading from '../Layouts/Loading';
 
 export default class PageTemplate extends Component {
@@ -33,13 +32,14 @@ export default class PageTemplate extends Component {
         };
 
         const { data, isLoading } = this.state;
+        const { ContentWrapper } = this.props;
 
         return (
             <>
                 {isLoading ? (
                     <Loading />
                 ) : (
-                    <ContentBlock
+                    <ContentWrapper
                         dangerouslySetInnerHTML={createMarkup(
                             data.content.rendered
                         )}
@@ -50,34 +50,6 @@ export default class PageTemplate extends Component {
     }
 }
 
-const ContentBlock = styled.div`
-    padding: 0 5px;
-    width: calc(100% - 10px);
-    text-align: justify;
-
-    ${({ theme }) => theme.media.above.sm} {
-        padding: 0 15px;
-        width: 100%;
-    }
-
-    img {
-        max-width: 100%;
-    }
-
-    li {
-        text-align: center;
-
-        ${({ theme }) => theme.media.above.sm} {
-            display: inline-block;
-            width: 50%;
-
-            :first-child {
-                padding-right: 10px;
-            }
-
-            :nth-child(even) {
-                padding-left: 10px;
-            }
-        }
-    }
-`;
+PageTemplate.defaultProps = {
+    ContentWrapper: 'div',
+};
