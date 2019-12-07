5a2729e03df4810728e78dbb758aff95fe3eb6e7
Neology92 <oskarlegner@gmail.com>
2019-12-07 03:32:32 +0100

    Create program page

--- src/Layouts/Program.js
+++ src/Layouts/Program.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import PageTemplate from '../Templates/PageTemplate';
 
 const Program = () => {
-    return <div>Program</div>;
+    return (
+        <div>
+            <PageTemplate slug="program-dorosli" />
+            <PageTemplate slug="program-dzieci" />
+            <PageTemplate slug="imprezy-dodatkowe" />
+        </div>
+    );
 };
 
 export default Program;
