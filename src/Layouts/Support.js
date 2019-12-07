e495bcc3b16ddacdb990adc51c90b9cf4c707f11
Neology92 <oskarlegner@gmail.com>
2019-12-07 04:39:02 +0100

    Add info

--- src/Layouts/Support.js
+++ src/Layouts/Support.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Support = () => {
-    return <div>Wpłać na WOŚP</div>;
+    return (
+        <div style={{ fontSize: '17px' }}>
+            <p>Wpłacanie będzie możliwe już niedługo! Śledźcie uważnie!</p>
+            <p>
+                Gramy do końca świata i o jeden dzień dłużej!
+                <br />
+                Sie ma!
+            </p>
+        </div>
+    );
 };
 
 export default Support;
