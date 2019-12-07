d3bff874e451104821874ec2b141b9e530d54877
Kubeł <jkurczew@gmail.com>
2019-12-07 16:29:57 +0100

    Sponsor logo on button

--- src/Layouts/Home.js
+++ src/Layouts/Home.js
@@ -43,7 +43,7 @@ const Home = () => {
                         <PostCardGrid />
                     </Col>
                     <Col md={12} xl={4} as="aside">
-                        <MainSponsor />
+                        {/* <MainSponsor /> */}
                         <FacebookWidget />
                     </Col>
                 </Row>
